c5a9a55d84328ba412489393b01ee42a21bc98dc
Starchery <pivotsallit@gmail.com>
2020-04-16 05:57:49 -0400

    working with slightly harder test program

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
 
-    let program = fs::read_to_string("tests/test0.lang")?;
+    let program = fs::read_to_string("tests/test1.lang")?;
 
     let mut lex = lexer::Lexer::new(&program);
     lex.tokenize();
