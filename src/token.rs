b15075ed6253c0b06264a14b0250789c9480c3d3
Starchery <pivotsallit@gmail.com>
2020-05-17 14:21:57 -0400

    *doesnt build* tweaking regexes

--- src/token.rs
+++ src/token.rs
@@ -2,21 +2,23 @@
 
 use logos::Logos;
 use logos::source::Source;
+
 use std::ops::Range;
 use std::fmt;
 
 trait Lexeme { }
 
 #[derive(Logos, Debug, PartialEq)]
-pub(crate) enum Token<'src> {
+pub(crate)
+enum Token<'src> {
 
     Sym(Symbol),
 
-    Key(Keyword),
+    // Key(Keyword),
 
     Lit(Literal<'src>),
 
-    #[regex(r"[a-zA-Z]+")]
+    #[regex("[a-zA-Z_$][a-zA-Z0-9_$]*")]
     Id(&'src str),
 
     #[regex(r"\p{White_Space}", logos::skip)]
@@ -26,56 +28,138 @@ pub(crate) enum Token<'src> {
     InvalidToken
 }
 
-#[derive(Debug, PartialEq)]
-pub(crate) enum Keyword {
-    Type,
-    Where
-    , If,   Else
-    , Let,  Match
-    , In,   Trait
-    , Impl, For
-    , Then, Deriving
-    , As,   Alias
-    }
+// #[derive(Debug, PartialEq)]
+// pub(crate)
+// enum Keyword {
+//     Type,
+//     Where
+//     , If,   Else
+//     , Let,  Match
+//     , In,   Trait
+//     , Impl, For
+//     , Then, Deriving
+//     , As,   Alias
+//     }
 
-#[derive(Debug, PartialEq)]
-pub(crate) enum Literal<'src>
-    { Str(&'src str)
-    , FStr(&'src str)
-    , Int(i64)
-    , Float(f64)
-    }
+#[derive(Logos, Debug, PartialEq)]
+pub(crate)
+enum Literal<'src> {
+    #[regex(r#""([^"\\]|\\t|\\u|\\n|\\")*""#)]
+    Str(&'src str),
 
-#[derive(Debug, PartialEq)]
-pub(crate) enum Symbol
+    #[regex(r#"f"([^"\\]|\\t|\\u|\\n|\\")*""#)]
+    FStr(&'src str),
+
+    #[regex("[0-9]*[0-9]+([eE][+-]?[0-9]+)?|[0-9]+[eE][+-]?[0-9]+",
+      |lex| lex.slice().parse())]
+    Int(i64),
+
+    #[regex("[0-9]*\\.[0-9]+([eE][+-]?[0-9]+)?|[0-9]+[eE][+-]?[0-9]+",
+      |lex| lex.slice().parse())]
+    Float(f64),
+}
+
+#[derive(Logos, Debug, PartialEq)]
+pub(crate)
+enum Symbol {
     // delimiters
-   { LParen,  RParen
-   , LCurly,  RCurly
-   , LBrack,  RBrack
-   , Comma ,  Dot
+    #[token("(")]
+    LParen,
+
+    #[token(")")]
+    RParen,
+
+    #[token("{")]
+    LCurly,
+
+    #[token("}")]
+    RCurly,
+
+    #[token("[")]
+    LBrack,
+
+    #[token("]")]
+    RBrack,
+
+    #[token(",")]
+    Comma,
+
+    #[token(".")]
+    Dot,
 
     // single-char
-   , Dash,  Equals
-   , Plus,  Lambda
-   , Star,  Slash
-   , Pipe,  Quote
-   , Semi,  Apostrophe
-   , Hash,  Colon
-   , Lt,    Gt
+    #[token("-")]
+    Dash,
 
-    // multi-char
-   , Arrow, Colons
-   , EqEq,  Leq
-   , Geq,   DashDash
-   , FatArrow
+    #[token("=")]
+    Equals,
+
+    #[token("+")]
+    Plus,
+
+    #[token("\\")]
+    Lambda,
+
+    #[token("*")]
+    Star,
+
+    #[token("/")]
+    Slash,
+
+    #[token("|")]
+    Pipe,
+
+    #[token("\"")]
+    Quote,
+
+    #[token(";")]
+    Semi,
+
+    #[token("'")]
+    Apostrophe,
+
+    #[token("#")]
+    Hash,
+
+    #[token(":")]
+    Colon,
+
+    #[token("<")]
+    Lt,
+
+    #[token(">")]
+    Gt,
+
+     // multi-char
+    #[token("->")]
+    Arrow,
+
+    #[token("::")]
+    Colons,
+
+    #[token("==")]
+    EqEq,
+
+    #[token("<=")]
+    Leq,
+
+    #[token(">=")]
+    Geq,
+
+    #[token("--")]
+    DashDash,
+
+    #[token("=>")]
+    FatArrow,
 }
 
 impl Lexeme for Symbol {}
 impl<'src> Lexeme for Literal<'src> {}
-impl Lexeme for Keyword {}
+// impl Lexeme for Keyword {}
 impl<'src> Lexeme for Token<'src> {}
 
-pub(crate) fn assert_lex<'a, Token>(
+pub(crate)
+fn assert_lex<'a, Token>(
     source: &'a Token::Source,
     tokens: &[(Token, &'a <Token::Source as Source>::Slice, Range<usize>)],
 ) where
